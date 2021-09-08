de95ad308b3fef4b740dc02168b0fa710491b3b5
RihardXXX <yusupa.akaeva@yandex.ru>
2021-09-08 21:13:49 +0300

    add react

---
+++ app/src/components/editor/editor.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Editor = () => {
+  return <h1>Hello baby</h1>;
+};
+
+export default Editor;
--- app/src/script.js
+++ app/src/script.js
@@ -1,47 +1,56 @@
-const getPageList = () => {
-  const listPage = document.querySelector('.list-page');
-  listPage.innerHTML = '';
-  // get
-  fetch('/engine/admin/api')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      data.forEach((file) => {
-        const li = document.createElement('li');
-        li.innerHTML = file;
-        listPage.appendChild(li);
-      });
-    });
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-getPageList();
+import Editor from './components/editor';
 
-//post
-// Пример отправки POST запроса:
-async function postData(url = '', data) {
-  // Default options are marked with *
+ReactDOM.render(<Editor />, document.getElementById('root'));
 
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    body: data, // body data type must match "Content-Type" header
-  });
-  if (!response.ok) {
-    alert('Страница уже существует');
-  }
-  console.log(response);
-  return await response.text(); // parses JSON response into native JavaScript objects
-}
+// const getPageList = () => {
+//   const listPage = document.querySelector('.list-page');
+//   listPage.innerHTML = '';
+//   // get
+//   fetch('/engine/admin/api')
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       data.forEach((file) => {
+//         const li = document.createElement('li');
+//         li.innerHTML = file;
+//         listPage.appendChild(li);
+//       });
+//     });
+// };
 
-const button = document.querySelector('button');
-const namePage = document.querySelector('input');
+// getPageList();
 
-// создание страниц
-button.addEventListener('click', () => {
-  const formData = new FormData();
-  formData.append('page', namePage.value);
-  namePage.value = '';
-  postData('/engine/admin/api/createNewPage.php', formData).then((data) => {
-    getPageList();
-  });
-});
+// //post
+// // Пример отправки POST запроса:
+// async function postData(url = '', data) {
+//   // Default options are marked with *
+
+//   const response = await fetch(url, {
+//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     body: data, // body data type must match "Content-Type" header
+//   });
+//   if (!response.ok) {
+//     alert('Страница уже существует');
+//   }
+//   console.log(response);
+//   return await response.text(); // parses JSON response into native JavaScript objects
+// }
+
+// const button = document.querySelector('button');
+// const namePage = document.querySelector('input');
+
+// // создание страниц
+// button.addEventListener('click', () => {
+//   const formData = new FormData();
+//   formData.append('page', namePage.value);
+//   namePage.value = '';
+//   postData('/engine/admin/api/createNewPage.php', formData).then((data) => {
+//     getPageList();
+//   });
+// });
+
+// console.log(111);
